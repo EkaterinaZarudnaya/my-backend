7b883d608755ecdd0cad202497ded21d4390a716
EkaterinaZarudnaya <zarudnayakatyaa@gmail.com>
2022-09-19 11:17:40 +0300

    fixes + go:embed

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	_ "embed"
 	"my-backend/server"
 )
+
+//go:embed templates/index.html
+var IndexHtml string
 
 func main() {
 	server.RouteHandler()
--- server/handlers/upload.go
+++ server/handlers/upload.go
@@ -1,13 +1,24 @@
 package handlers
 
 import (
+	_ "embed"
 	"fmt"
 	"html/template"
+	"io"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
+
+//go:embed templates/index.html
+var indexHtml string
 
 func Upload(w http.ResponseWriter, req *http.Request) {
 
+	//templateFile := template.Must(template.New("index.html").Parse(indexHtml))
 	templateFile := template.Must(template.ParseFiles("templates/index.html"))
 
 	if req.Method == http.MethodPost {
@@ -20,7 +31,11 @@ func Upload(w http.ResponseWriter, req *http.Request) {
 
 func handleUpload(w http.ResponseWriter, req *http.Request) {
 
-	req.ParseMultipartForm(10 << 20)
+	if e := req.ParseMultipartForm(10 << 20); e != nil {
+		http.Error(w, e.Error(), http.StatusBadRequest)
+		fmt.Println("parse err", e.Error())
+		return
+	}
 
 	file, fileHeader, err := req.FormFile("file")
 	if err != nil {
@@ -31,7 +46,29 @@ func handleUpload(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Retrieving the file successfully.")
 	defer file.Close()
 
-	FileHandler(fileHeader.Filename, file, w)
+	saveFile(fileHeader.Filename, file, w)
 
 	http.Redirect(w, req, "/?success=true", http.StatusSeeOther)
+}
+
+func saveFile(Filename string, file multipart.File, w http.ResponseWriter) {
+	dt := time.Now().Format("2006-01-02T15.04")
+	name := strings.TrimSuffix(Filename, filepath.Ext(Filename))
+	ext := filepath.Ext(Filename)
+
+	saveName := name + dt + ext
+	filePath := filepath.Join("uploads", saveName)
+
+	dest, err := os.Create(filePath)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	defer dest.Close()
+
+	if _, err = io.Copy(dest, file); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("The file was saved successfully.")
 }
